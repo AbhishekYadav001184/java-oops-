6a5772625747218e88a92253b618dfaa00501891
Abhishek Yadav <o111ay7392@gmail.com>
2025-09-05 09:54:16 +0530

    First commit

---
+++ Desktop/"A B C D"/video 1/All.java
@@ -0,0 +1,168 @@
+// question 1
+
+// public class All {
+//     public static void main(String[] args){
+//         for(int i=1;i<=10;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// question 2
+
+// import java.util.Scanner;
+// public class All{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter the N");
+//         int N=sc.nextInt();
+//         for( int i  =1; i<=N;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+//quetion 3
+
+// import java.util.Scanner;
+// public class All{
+//     public static void main(String[] args){
+//       Scanner sc=new Scanner(System.in);
+//       System.out.println("enter the no. A");
+//       int A=sc.nextInt();
+//       System.out.println("enter the no. B");
+//       int B=sc.nextInt();  
+//       for(int i=A; i<=B;i++){
+//         System.out.print(i+" ");
+//       }
+
+//     }
+// }
+
+// question 4
+
+// import java.util.Scanner;
+// public class All{
+//      public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter the no. A");
+//        int A=sc.nextInt();
+//        System.out.println("enter the no. B");
+//        int B=sc.nextInt();  
+//        for(int i=A; i<=B;i++){
+//             if(i%2==0){
+//                 System.out.print(i+ " ");
+//             }
+//        }
+//      }
+//     }                
+
+
+// question 5
+
+
+// import java.util.Scanner;
+//  public class All{
+//       public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter the no. A");
+//         int A=sc.nextInt();
+//         System.out.println("enter the no. B");
+//         int B=sc.nextInt(); 
+//          int sum=0;
+//         for(int i=A; i<=B;i++){
+//              if(i%2==0){
+//                  sum=sum+i;
+//              }
+//         }
+//         System.out.print(sum);
+//         sc.close();
+//       }
+//      }                
+
+// question 6
+
+
+// import java.util.Scanner;
+// public class All{
+//     public static void main(String[] args){
+//       Scanner sc=new Scanner(System.in);
+//       System.out.println("enter the no. A");
+//       int A=sc.nextInt();
+//       System.out.println("enter the no. B");
+//       int B=sc.nextInt();  
+//       int sum=0;
+//       for(int i=A; i<=B;i++){
+//         sum=sum+i;
+//       }
+//       System.out.print(sum+" ");
+//     }
+// }
+
+// question 7
+
+// import java.util.Scanner;
+// public class All{
+//     public static void main(String[] args){
+//       Scanner sc=new Scanner(System.in);
+//       System.out.println("enter the no. A");
+//       int A=sc.nextInt();
+//       System.out.println("enter the no. B");
+//       int B=sc.nextInt();  
+//       for(int i=B; i>=A;i--){
+//         System.out.print(i+" ");
+//       }
+
+//     }
+// }
+
+
+// question 8
+
+// import java.util.Scanner;
+// public class All{
+//     public static void main(String[] args){
+//       Scanner sc=new Scanner(System.in);
+//       System.out.println("enter the no. A");
+//       int A=sc.nextInt();
+//       System.out.println("enter the no. B");
+//       int B=sc.nextInt();  
+//       for(int i=B; i>=A;i--){
+//         if(i%2==0){
+//             System.out.print(i+" ");
+//         }
+//       }
+
+//     }
+// }
+
+// question 9
+
+// sum of even and multi of odd from a to b
+
+
+
+// import java.util.Scanner;
+// public class All{
+//     public static void main(String[] args){
+//       Scanner sc=new Scanner(System.in);
+//       System.out.println("enter the no. A");
+//       int A=sc.nextInt();
+//       System.out.println("enter the no. B");
+//       int B=sc.nextInt(); 
+//       int sum=0;
+//       long mult=1; 
+//       for(int i=A; i<=B;i++){
+//         if(i%2==0){
+//             sum=sum+i;
+//         }
+//         else{
+//             mult=mult*i;
+//         }
+//       }
+//       System.out.println(sum);
+//       System.out.println(mult);
+
+//     }
+// }
+
